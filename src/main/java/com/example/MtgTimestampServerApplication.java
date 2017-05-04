0b56f57136e6d0b3871505d1209eb4732aeea383
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-05-05 07:51:10 +0900

    add RestController and entity

--- src/main/java/com/example/MtgTimestampServerApplication.java
+++ src/main/java/com/example/MtgTimestampServerApplication.java
@@ -1,12 +1,32 @@
 package com.example;
 
+import com.example.entity.LapInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class MtgTimestampServerApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(MtgTimestampServerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MtgTimestampServerApplication.class, args);
 	}
+
+	@PostMapping(value = "/POST")
+//    @RequestMapping(method = RequestMethod.POST, value = "/POST")
+	public HttpStatus postSlack(@RequestBody LapInfo lapInfo) {
+		log.info(lapInfo.toString());
+		return HttpStatus.OK;
+	}
+
+    @RequestMapping(method = RequestMethod.GET, value = "/GET")
+    public HttpStatus get() {
+        return HttpStatus.OK;
+    }
 }
---
+++ src/main/java/com/example/entity/LapInfo.java
@@ -0,0 +1,11 @@
+package com.example.entity;
+
+import lombok.Data;
+
+/**
+ * Created by naonaoakiaki on 2017/05/05.
+ */
+@Data
+public class LapInfo {
+    private String text;
+}
